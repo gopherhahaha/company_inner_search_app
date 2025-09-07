0137c16551b3f05cd8b0d27c0fb6da9d33b2589d
gopherhahaha <gopherhahaha@gmail.com>
2025-09-08 06:14:09 +0900

    question3

--- components.py
+++ components.py
@@ -23,37 +23,51 @@ def display_app_title():
 
 def display_select_mode():
     """
-    サイドバーに回答モードのラジオボタンを表示
+    回答モードのラジオボタンを表示
     """
-    # 「サイドバー」内にラジオボタンを配置
-    with st.sidebar:
-        st.session_state.mode = st.radio(
-            label="利用目的",
-            options=[ct.ANSWER_MODE_1, ct.ANSWER_MODE_2]
-        )
+    st.sidebar.title("利用目的")
+    st.session_state.mode = st.sidebar.radio(
+    label="利用目的",
+    options=[ct.ANSWER_MODE_1, ct.ANSWER_MODE_2],
+    label_visibility="collapsed"
+    )
 
+
+# def display_initial_ai_message():
+#     """
+#     AIメッセージの初期表示
+#     """
+#     with st.chat_message("assistant"):
+#         # 「st.success()」とすると緑枠で表示される
+#         st.markdown("こんにちは。私は社内文書の情報をもとに回答する生成AIチャットボットです。上記で利用目的を選択し、画面下部のチャット欄からメッセージを送信してください。")
+
+#         # 「社内文書検索」の機能説明
+#         st.markdown("**【「社内文書検索」を選択した場合】**")
+#         # 「st.info()」を使うと青枠で表示される
+#         st.info("入力内容と関連性が高い社内文書のありかを検索できます。")
+#         # 「st.code()」を使うとコードブロックの装飾で表示される
+#         # 「wrap_lines=True」で折り返し設定、「language=None」で非装飾とする
+#         st.code("【入力例】\n社員の育成方針に関するMTGの議事録", wrap_lines=True, language=None)
+
+#         # 「社内問い合わせ」の機能説明
+#         st.markdown("**【「社内問い合わせ」を選択した場合】**")
+#         st.info("質問・要望に対して、社内文書の情報をもとに回答を得られます。")
+#         st.code("【入力例】\n人事部に所属している従業員情報を一覧化して", wrap_lines=True, language=None)
 
 def display_initial_ai_message():
     """
     AIメッセージの初期表示
     """
     with st.chat_message("assistant"):
-        # 「st.success()」とすると緑枠で表示される
-        st.markdown("こんにちは。私は社内文書の情報をもとに回答する生成AIチャットボットです。上記で利用目的を選択し、画面下部のチャット欄からメッセージを送信してください。")
-
-        # 「社内文書検索」の機能説明
-        st.markdown("**【「社内文書検索」を選択した場合】**")
-        # 「st.info()」を使うと青枠で表示される
-        st.info("入力内容と関連性が高い社内文書のありかを検索できます。")
-        # 「st.code()」を使うとコードブロックの装飾で表示される
-        # 「wrap_lines=True」で折り返し設定、「language=None」で非装飾とする
-        st.code("【入力例】\n社員の育成方針に関するMTGの議事録", wrap_lines=True, language=None)
-
-        # 「社内問い合わせ」の機能説明
-        st.markdown("**【「社内問い合わせ」を選択した場合】**")
-        st.info("質問・要望に対して、社内文書の情報をもとに回答を得られます。")
-        st.code("【入力例】\n人事部に所属している従業員情報を一覧化して", wrap_lines=True, language=None)
-
+        # st.markdown("こんにちは。私は社内文書の情報をもとに回答する生成AIチャットボットです。サイドバーで利用目的を選択し、画面下部のチャット欄からメッセージを送信してください。")
+        st.success("**こんにちは。私は社内文書の情報をもとに回答する生成AIチャットボットです。サイドバーで利用目的を選択し、画面下部のチャット欄からメッセージを送信してください。**")
+        st.warning("**具体的に入力したほうが期待通りの回答を得やすいです。**")
+        st.sidebar.markdown("##### 【「社内文書検索」を選択した場合】")
+        st.sidebar.info("入力内容と関連性が高い社内文書のありかを検索できます。")
+        st.sidebar.code("【入力例】\n社員の育成方針に関するMTGの議事録", language=None)
+        st.sidebar.markdown("##### 【「社内問い合わせ」を選択した場合】")
+        st.sidebar.info("質問・要望に対して、社内文書の情報をもとに回答を得られます。")
+        st.sidebar.code("【入力例】\n人事部に所属している従業員情報を\n一覧化して", language=None)
 
 def display_conversation_log():
     """
